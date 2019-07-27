17d3cfa03b5ebb50c0140631688cd6c0595e0d84
Kevin Huo <khuo002@wch136-05.cs.ucr.edu>
2019-07-27 16:59:03 -0700

    Shoulda added both you fool

--- Lab5/header/io.h
+++ Lab5/header/io.h
@@ -4,7 +4,9 @@
 void LCD_init();
 void LCD_ClearScreen(void);
 void LCD_WriteCommand (unsigned char Command);
+void LCD_WriteData(unsigned char Data);
 void LCD_Cursor (unsigned char column);
 void LCD_DisplayString(unsigned char column,const unsigned char *string);
 void delay_ms(int miliSec);
+void LCD_Cust_Char(unsigned char loc, unsigned char *msg);
 #endif
